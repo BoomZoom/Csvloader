1fb5b81e505be17eaa8ef61330ecc01ecabda8d0
Andrey <andrey.venzhega@gmail.com>
2018-10-18 15:35:17 +0300

    Create class Product

---
+++ ConsoleApp35/ConsoleApp35/Product.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp35
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public int ProviderId { get; set; }
+        public int CattegoryId { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public int Quantity { get; set; }
+        public DateTime DateCreationTime { get; set; }
+        public DateTime DateUpdateTime { get; set; }
+
+    }
+}
